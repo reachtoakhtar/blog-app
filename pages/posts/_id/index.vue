5e460b8c2646a6c803b89592216d31dc9b0ded54
Akhtar <akhtar.husain@nirvedacognition.ai>
2019-04-22 17:11:47 +0530

    - Added blog posts.

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,75 @@
+<!-- Created by Akhtar on 22/04/19 -->
+
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">Title of the post</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on: xxx</div>
+        <div class="post-detail">Written By: Name</div>
+      </div>
+      <p class="post-content">Content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p>Let me know what you think about the post.</p>
+      <p>
+        Send a mail to
+        <a href="mail:feedback@mycompany.com">feedback@mycompany.com</a>
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<!-- Created by Akhtar on 22/04/19 -->
+
+<template>
+  <div class="posts-page">Post List</div>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
